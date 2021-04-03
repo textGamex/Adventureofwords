b1b011b88cb84467fb8424e0f9af2a0fbe77fe61
Millennium <2478444358@qq.com>
2021-04-03 17:28:18 +0800

    通过调用C,实现控制台清屏

---
+++ src/C/Cls-dll.c
@@ -0,0 +1,8 @@
+#include "javaLogic_Tools_GameTool.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+JNIEXPORT void JNICALL Java_javaLogic_Tools_GameTool_cls(JNIEnv * as, jclass ss)
+{
+    system("cls");
+}
---
+++ src/ui/LoginUi.java
@@ -0,0 +1,51 @@
+package ui;
+
+import javaLogic.Account.AccountMessage;
+
+import java.util.Scanner;
+
+public class LoginUi
+{
+    public static void main(String[] args)
+    {
+        System.out.print("请输入账号: ");
+        var account = AccountMessage.readAccount(new Scanner(System.in));
+//        var account = in.nextLine();
+//        var id = AccountModule(account);
+        switch (account.getId())
+        {
+            case NEWGM:
+            case NEWPLAYER:
+                System.out.println("您好!新玩家");
+                account.createAccountDataFolder();
+                break;
+            case GM:
+            case PLAYER:
+                System.out.println("登录成功!");
+                System.out.println("欢迎您再次来到文字冒险的世界");
+                break;
+
+        }
+    }
+    public static AccountMessage loginMain()
+    {
+        System.out.print("请输入账号: ");
+        var account = AccountMessage.readAccount(new Scanner(System.in));
+//        var account = in.nextLine();
+//        var id = AccountModule(account);
+        switch (account.getId())
+        {
+            case NEWGM:
+            case NEWPLAYER:
+                System.out.println("您好!新玩家");
+                account.createAccountDataFolder();
+                break;
+            case GM:
+            case PLAYER:
+                System.out.println("登录成功!");
+                System.out.println("欢迎您再次来到文字冒险的世界");
+                break;
+        }
+        return account;
+    }
+}
