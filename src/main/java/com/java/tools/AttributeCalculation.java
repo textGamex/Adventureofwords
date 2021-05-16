3c57f3633e9c28a4dfe96342278fa5a18edcd81a
Millennium <1543255825@qq.com>
2021-05-16 10:13:38 +0800

    进一步完善数值计算模块
    Signed-off-by: Millennium <1543255825@qq.com>

--- src/main/java/com/java/tools/AttributeCalculation.java
+++ src/main/java/com/java/tools/AttributeCalculation.java
@@ -5,17 +5,19 @@ import static java.util.Objects.requireNonNull;
 
 /**
  * @author 千年
- * @version 1.0.1
+ * @version 1.0.2
+ * @since 20221-5-15
 */
 public final class AttributeCalculation
 {
     public static void main(String[] args)
     {
-//        System.out.println(victimEvade(1, -1));
-//        System.out.println(attackerCritChance(1, 0));
+        System.out.println(victimEffectiveHp(100, 1.9, 0.98));
     }
 
+
     /**
+     * 计算攻击者的攻击未命中的几率.
      *
      * @param attacker 攻击者
      * @param victim 被攻击者
@@ -33,6 +35,7 @@ public final class AttributeCalculation
     }
 
     /**
+     * 根据给定的数值计算攻击者的攻击未命中的几率.
      *
      * @param attackerHit 攻击者的命中
      * @param victimEvade 被攻击者的闪避
@@ -84,22 +87,31 @@ public final class AttributeCalculation
     }
 
     /**
+     * 计算攻击者能对被攻击者打出的伤害.
      *
-     * @param attacker
-     * @param victim
+     * @param attacker 攻击者
+     * @param victim 被攻击者
      * @throws NullPointerException 如果{@code attacker}或{@code victim}为null
-     * @return
+     * @return 攻击者的伤害
      */
-    public static int attackerPhysicalDamage(BasicUnit attacker, BasicUnit victim)
+    public static double attackerPhysicalDamage(BasicUnit attacker, BasicUnit victim)
     {
         return attackerPhysicalDamage(requireNonNull(attacker).getPhysicalAttack(),
                 requireNonNull(victim).getArmor());
     }
 
-    public static int attackerPhysicalDamage(int attackerPhysicalAttack, int victimArmor)
+    /**
+     * 用给定的数值计算攻击者能对被攻击者打出的伤害.
+     *
+     * @param attackerPhysicalAttack 攻击者的物理攻击
+     * @param victimArmor 被攻击者的护甲值
+     * @throws NullPointerException 如果{@code attacker}或{@code victim}为null
+     * @return 攻击者的伤害
+     */
+    public static double attackerPhysicalDamage(double attackerPhysicalAttack, double victimArmor)
     {
         var attack = attackerPhysicalAttack;
-        var armor = attackerPhysicalAttack;
+        var armor = victimArmor;
 
         //为了防止出现NaN错误
         if (attackerPhysicalAttack + victimArmor == 0)
@@ -110,5 +122,55 @@ public final class AttributeCalculation
                 armor = ++victimArmor;
         }
         return attack * attack / (attack + armor);
+    }
+
+    /**
+     * 计算每回合伤害.
+     *
+     * @param attacker 攻击者
+     * @param victim 被攻击者
+     * @throws NullPointerException 如果{@code attacker}或{@code victim}为null
+     */
+    public double DPR(BasicUnit attacker, BasicUnit victim)
+    {
+        var damage = attackerPhysicalDamage(attacker, victim);
+        var critChance = attackerCritChance(attacker, victim);
+        return damage * (1 - critChance) + damage * critChance * attacker.getCritsEffect();
+    }
+
+    /**
+     * 计算被攻击者的有效生命值, 如果被攻击者的闪避率或伤害减免大于等于100%, 返回{@code Integer.MAX_VALUE}.
+     *
+     * @param attacker 攻击者
+     * @param victim 被攻击者
+     * @return 返回被攻击者的有效生命值
+     * @throws NullPointerException 如果{@code attacker}或{@code victim}为null
+     */
+    public static double victimEffectiveHp(BasicUnit attacker, BasicUnit victim)
+    {
+        var physicalAttack = requireNonNull(attacker).getPhysicalAttack();
+        if (physicalAttack == 0)
+            ++physicalAttack;
+
+        var damage = attackerPhysicalDamage(attacker, victim);
+        var damageReduction = (physicalAttack - damage) / physicalAttack;
+        var evadeChance = victimEvade(attacker, victim);
+        if (evadeChance >= 1.0 || damageReduction >= 1.0)
+            return Integer.MAX_VALUE;
+        return victim.getMaxHp() / (1.0 - damageReduction) / (1.0 - evadeChance);
+    }
+
+    /**
+     *
+     * @param victimHp
+     * @param damageReduction
+     * @param evadeChance
+     * @return
+     */
+    public static double victimEffectiveHp(int victimHp, double damageReduction, double evadeChance)
+    {
+        if (evadeChance >= 1.0 || damageReduction >= 1.0)
+            return Integer.MAX_VALUE;
+        return victimHp / (1.0 - damageReduction) / (1.0 - evadeChance);
     }
 }
